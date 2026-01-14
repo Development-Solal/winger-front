4b3ad28b9c60dbfb73b2add0fffea290ccad745f
Teshand <teshand.r@indigita.ch>
2026-01-14 15:30:01 +0400

    fix vite seo build

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import prerender from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    prerender({
-      staticDir: path.resolve(__dirname, "dist"),
-
-      // routes to prerender
-      routes: [
-        "/",
-        "/profilsAidantAide",
-        "/concept",
-        "/rendezvous",
-        "/tarifs",
-        "/espacePro",
-        "/qui-sommes-nous"
-      ]
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: "es2018"
+    target: "es2018" // or "es2015" for max compatibility
   }
 });
